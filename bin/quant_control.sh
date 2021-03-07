d46cf180d7f29b18519c01ccaefa5b6836adaa8b
Zhang <www.zhangyunsheng.com@gmail.com>
2021-03-08 03:44:11 +0800

    modify bin

--- bin/quant_control.sh
+++ bin/quant_control.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-#****************************************************************#
-# @Brief: quant_control.sh
-# @Author: www.zhangyunsheng.com@gmail.com
-# @CreateDate: 2017-04-06 01:22
-# @ModifyDate: 2017-04-16 23:10
-# Copyright ? 2017 Baidu Incorporated. All rights reserved.
-#***************************************************************#
 
-bin=`dirname $(readlink -f $0)`
+PY='python3'
+
+os='Linux'
+if [ "$(uname)" == "Darwin" ];then
+    os='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];then
+    os='Linux'
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ];then
+    os='Windows'
+fi
+
+#bin=`dirname $(readlink -f $0)`
+bin=`dirname $0`
 root=$bin"/../"
 
 INTERFACE=$1
@@ -17,8 +22,15 @@ case "$INTERFACE" in
     start)
         START_PARA=$*
         echo "start"
-        #python $root"/quant/quant.py" >>$root"/log/out.log" 2>&1 &
-        $bin"/daemontools/command/supervise" $bin >>$root"/log/supervise.log" 2>&1 &
+
+        if [ $os == "Linux" ];then
+            ${PY} $root"/quant/quant.py" 2>&1 | awk '{print "["strftime("%F %H:%M:%S")"]"$0}' >>$root"/log/out.log" &
+        elif [ $os == "Mac" ];then
+            ${PY} $root"/quant/quant.py" 2>&1 | awk '{system("date"); print}' >>$root"/log/out.log" &
+        fi
+        #${PY} $root"/quant/quant.py" >>$root"/log/out.log" 2>&1 &
+
+        #$bin"/daemontools/command/supervise" $bin >>$root"/log/supervise.log" 2>&1 &
         RET=$?
         exit $RET
         ;;
